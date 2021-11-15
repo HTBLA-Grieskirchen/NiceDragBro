ae52d5b6a7b3b8298f636b4238513a3c7aea0fa9
TimerErTim <timerertim@gmail.com>
2021-11-15 17:49:02 +0100

    Add Person class

---
+++ src/main/kotlin/at/htlgrieskirchen/nicedragbro/Person.kt
@@ -0,0 +1,11 @@
+package at.htlgrieskirchen.nicedragbro
+
+data class Person(
+    val id: Int,
+    val firstName: String,
+    val lastName: String,
+    val size: Double,
+    val weight: Double,
+    val bmi: Double,
+    val ffmi: Double
+)
